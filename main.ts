9a38ef231fa5b20fa6edba02b11d6f485245103a
ohioman709 <122560323+ohioman709@users.noreply.github.com>
2023-01-20 13:14:16 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,16 @@
 input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    music.playMelody("C C - - - - - - ", 500)
     basic.showIcon(IconNames.Square)
-    basic.pause(450)
+    music.playMelody("F F - - - - - - ", 500)
     basic.showIcon(IconNames.SmallSquare)
-    basic.pause(1000)
+    music.playMelody("A A - - - - - - ", 500)
     basic.showLeds(`
         . . . . .
         . . . . .
@@ -10,11 +18,80 @@ input.onButtonPressed(Button.A, function () {
         . . . . .
         . . . . .
         `)
-    basic.pause(350)
+    music.playMelody("C5 C5 - - - - - - ", 120)
 })
-basic.forever(function () {
-	
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    music.playMelody("C C - - - - - - ", 500)
+    basic.showIcon(IconNames.Square)
+    music.playMelody("F F - - - - - - ", 500)
+    basic.showIcon(IconNames.SmallSquare)
+    music.playMelody("A A - - - - - - ", 500)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    music.playMelody("C5 C5 - - - - - - ", 500)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    music.playMelody("C5 C5 - - - - - - ", 500)
+    basic.showIcon(IconNames.SmallSquare)
+    music.playMelody("A A - - - - - - ", 500)
+    basic.showIcon(IconNames.Square)
+    music.playMelody("F F - - - - - - ", 500)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    music.playMelody("C C - - - - - - ", 118)
 })
-basic.forever(function () {
-	
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    music.playMelody("C5 C5 - - - - - - ", 500)
+    basic.showIcon(IconNames.SmallSquare)
+    music.playMelody("A A - - - - - - ", 500)
+    basic.showIcon(IconNames.Square)
+    music.playMelody("F F - - - - - - ", 500)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    music.playMelody("C C - - - - - - ", 118)
 })
+input.onGesture(Gesture.Shake, function () {
+    music.playMelody("C5 F C5 F C5 F C5 F ", 378)
+    music.playMelody("C5 F C5 F C5 F C5 F ", 378)
+})
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
+    basic.showString("Hello!")
+})
+music.playMelody("A C5 E G A B G D ", 400)
+music.playMelody("E G D B F A E C5 ", 400)
+music.playMelody("A C5 E G A B G D ", 400)
+music.playMelody("E G D B F A E C5 ", 400)
